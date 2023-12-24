fa2538b0d78a61fd291eca61a5df7c2af3bca783
Hanshi <hanshitrash00@proton.me>
2023-12-25 07:44:31 +0800

    docs(urls): add to-do

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ from .views import AppView
 urlpatterns = [
     path("", AppView.as_view(), name="app"),
     path("products/", include("products.urls")),
+    # TODO: add more applications url when it's already configured.
     path("admin/", admin.site.urls),
 ]
