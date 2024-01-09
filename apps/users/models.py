141d8b853a7e886e227cdb6001f3f0e4e4de0d45
Hanshi <hanshitrash00@proton.me>
2024-01-10 07:12:51 +0800

    refactor(users): add UserProfile class to extend User model fields.

--- apps/users/models.py
+++ apps/users/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# class UserProfile(models.Model):
-#     user = models.OneToOneField(to=User, on_delete=models.CASCADE)
-#     location = models.CharField(
-#         max_length=255,
-#         blank=False,
-#         null=False,
-#     )
+class UserProfile(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    location = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+    )
